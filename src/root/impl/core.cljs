fa01d4caa74055467a154dce42abd963bb2b5e36
den1k <kalavox@gmail.com>
2019-03-23 19:47:36 -0400

    Add parent-id during resolution

--- src/root/impl/core.cljs
+++ src/root/impl/core.cljs
@@ -1,5 +1,6 @@
 (ns root.impl.core
   (:require [medley.core :as md]
+            [reagent.core :as r]
             [cljs.spec.alpha :as s]))
 
 ;; register/remove views
@@ -9,10 +10,12 @@
 
 (def data
   [{:id 1 :type :container :content [2]}
-   {:id 2 :type :toggle-list :properties {:name "Shopping List"} :content {:todos [3 4 5]}}
+   {:id 2 :type :toggle-list :attrs {:name "Shopping List"} :content {:new-todo 6
+                                                                      :todos    [3 4 5]}}
    {:id 3 :type :todo-item :markup ["Buy Bananas \uD83C\uDF4C️"]}
    {:id 4 :type :todo-item :markup ["Buy strawberries"]}
-   {:id 5 :type :todo-item :markup ["Buy Cabbage"]}])
+   {:id 5 :type :todo-item :markup ["Buy Cabbage"]}
+   {:id 6 :type :button :markup ["New Todo"]}])
 
 (s/def ::id integer?)
 (s/def ::type keyword?)
@@ -27,7 +30,7 @@
 
 (s/def ::entity (s/keys :req-un [::id] :opt-un [::type ::content ::markup ::attrs]))
 
-(def state (atom (into {} (map (fn [x] [(:id x) x])) data)))
+(def state (r/atom (into {} (map (fn [x] [(:id x) x])) data)))
 
 (defn id? [x] (integer? x))
 (defn lookup [id] (get @state id))
@@ -37,9 +40,9 @@
     (if (= ret ::s/invalid)
       (throw
        (ex-info "Value doesn't match spec"
-                {:value   x
-                 :spec    spec
-                 :explain (s/explain-str spec x)
+                {:value        x
+                 :spec         spec
+                 :explain      (s/explain-str spec x)
                  :explain-data (s/explain-data spec x)}))
       ret)))
 
@@ -82,21 +85,23 @@
    (cond-> ent content (update :content resolve-content f))))
 
 (defn resolved-view
-  ([{:as root :keys [root-id]}] (resolved-view root root-id))
-  ([{:as root :keys [dispatch-view lookup]} id]
+  ([{:as root :keys [root-id]}] (resolved-view root root-id nil))
+  ([root id] (resolved-view root id nil))
+  ([{:as root :keys [dispatch-view lookup]} id parent-id]
    (-> id
        lookup
-       (resolve-child-views #(resolved-view root %))
+       (cond-> parent-id (assoc :parent-id parent-id))
+       (resolve-child-views #(resolved-view root % id))
        dispatch-view)))
 
 (defn default-child-view [content]
   (case (meta content)
     ::entity content
-    ::entities (into [:div] content)
-    ::entity-map (into [:div]
+    ::entities (into [:div {:style {:padding-left 10}}] content)
+    ::entity-map (into [:div {:style {:padding-left 10}}]
                        (map
                         (fn [[k child-or-children]]
-                          [:div (name k)
+                          [:div (str (name k) ": ")
                            [default-child-view child-or-children]]))
                        content)))
 
@@ -120,12 +125,17 @@
                     :default-dispatch-fn default-view})
    opts))
 
+(def root (ui-root {:lookup         lookup
+                    :ent->view-name :type}))
+
+(let [{:keys [add-view]} root]
+  (add-view
+   :button
+   (fn [{:keys [markup]}]
+     [:button (first markup)])))
+
 
 (defn test-root [id]
-  (let [root (ui-root {:root-id        id
-                       :lookup         lookup
-                       :ent->view-name :type})]
-
-    (doto (resolved-view root)
-      js/console.log)))
+  (doto (resolved-view root id)
+    js/console.log))
 
